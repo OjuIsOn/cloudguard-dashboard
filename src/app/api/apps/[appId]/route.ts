eedeff1b16864876792be9ff4e498dbd0e8537a5
OjuIsOn <ojasvisingh542.os@gmail.com>
2025-07-18 01:20:46 +0530

    qwesxcvghjiuytr

--- src/app/api/apps/[appId]/route.ts
+++ src/app/api/apps/[appId]/route.ts
@@ -1,15 +1,14 @@
-import { type NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 import { connectDB } from "@/lib/db";
 import { App } from "@/models/app";
 
 export async function GET(
-  request: NextRequest,
-  context: { params: { appId: string } }
-): Promise<NextResponse> {
+  request: Request,
+  { params }: { params: { appId: string } }
+) {
   try {
     await connectDB();
-    const app = await App.findById(context.params.appId);
+    const app = await App.findById(params.appId);
     
     if (!app) {
       return NextResponse.json(
