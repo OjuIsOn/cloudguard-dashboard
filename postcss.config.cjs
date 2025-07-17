dc987f9f39396c8526e09297dab780ad8e90c70f
OjuIsOn <ojasvisingh542.os@gmail.com>
2025-07-18 00:31:02 +0530

    chaaaljabcas

---
+++ postcss.config.cjs
@@ -0,0 +1,26 @@
+// import tailwindcss from 'tailwindcss';
+// import autoprefixer from 'autoprefixer';
+
+// export default {
+//   plugins: [
+//     tailwindcss,
+//     autoprefixer,
+//   ],
+// };
+
+// postcss.config.mjs
+// export default {
+//   plugins: {
+//     // ✅ Use the package name as the key, not require()
+//     'tailwindcss': {},
+//     'autoprefixer': {}
+//   }
+// }
+
+// postcss.config.cjs
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  }
+}
